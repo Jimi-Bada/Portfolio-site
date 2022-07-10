ab8894dfc59531d0f2c442a541449af20499deda
Jimi-Bada <jimibadaadedamola@gmail.com>
2022-07-10 08:41:56 +0100

    new master

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// NavBar
+const navBar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+
+
+
+// window.onscroll = () => {
+//     navBar.classList.remove('active');
+// }
